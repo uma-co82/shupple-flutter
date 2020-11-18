7ac1963f5f9a3790a532915d557dd9b9f47a6678
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-18 21:46:45 +0900

    Update home.dart

--- lib/scene/home.dart
+++ lib/scene/home.dart
@@ -6,6 +6,30 @@ class HomeScene extends StatefulWidget {
 }
 
 class _HomeSceneState extends State<HomeScene> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {}
+}
+
+class PageWidget extends StatelessWidget {
+  final Color color;
+  final String title;
+
+  PageWidget({Key key, this.color, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+      ),
+    );
+  }
 }
