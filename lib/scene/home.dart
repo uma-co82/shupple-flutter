9fda85191c1661c0bc6477caabd8f0077c513746
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-18 21:47:24 +0900

    Update home.dart

--- lib/scene/home.dart
+++ lib/scene/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScene extends StatefulWidget {
   @override
@@ -8,8 +9,35 @@ class HomeScene extends StatefulWidget {
 class _HomeSceneState extends State<HomeScene> {
   int _currentIndex = 0;
 
+  final _pageWidgets = [
+    PageWidget(color: Colors.white, title: 'Home'),
+    PageWidget(color: Colors.blue, title: 'Album'),
+    PageWidget(color: Colors.orange, title: 'Chat'),
+  ];
+
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BottomNavigationBar'),
+      ),
+      body: _pageWidgets.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.photo_album), title: Text('Album')),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), title: Text('Chat')),
+        ],
+        currentIndex: _currentIndex,
+        fixedColor: Colors.blueAccent,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) => setState(() => _currentIndex = index);
 }
 
 class PageWidget extends StatelessWidget {
