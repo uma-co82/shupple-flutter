c8f5b93319b1fd5442cd3245884920e92100c60d
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-18 22:17:11 +0900

    Create index.dart

---
+++ lib/scene/signUp/index.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class SignUpScene extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUpScene> {
+  @override
+  Widget build(BuildContext context) {}
+}
