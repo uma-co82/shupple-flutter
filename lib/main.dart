b149f22466f837ccde0787eae8bd1ec4998a09ac
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-18 23:39:21 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shupple_flutter/model/route.dart';
 import 'package:shupple_flutter/scene/home/index.dart';
 import 'package:shupple_flutter/scene/signUp/index.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
