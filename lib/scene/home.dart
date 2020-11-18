4e0b7fbafe43919575fd02f4cc008c35b8a52e5b
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-18 21:45:58 +0900

    Update home.dart

---
+++ lib/scene/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScene extends StatefulWidget {
+  @override
+  _HomeSceneState createState() => _HomeSceneState();
+}
+
+class _HomeSceneState extends State<HomeScene> {
+  @override
+  Widget build(BuildContext context) {}
+}
