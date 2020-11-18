d59f65a45de15c6fecdbd621170acff740d7a9c3
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-18 23:18:13 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shupple_flutter/model/route.dart';
 import 'package:shupple_flutter/scene/home/index.dart';
+import 'package:shupple_flutter/scene/signUp/index.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      initialRoute: SRoute.home,
+      initialRoute: SRoute.signUp,
       routes: <String, WidgetBuilder>{
-        SRoute.home: (BuildContext ctx) => HomeScene()
+        SRoute.home: (BuildContext ctx) => HomeScene(),
+        SRoute.signUp: (BuildContext ctx) => SignUpScene()
       },
     );
   }
