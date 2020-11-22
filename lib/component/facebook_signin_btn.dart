39d479dd40cd0cdf95c4f9f51b1db4fede5957d7
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-22 15:25:49 +0900

    Update facebook_signin_btn.dart

--- lib/component/facebook_signin_btn.dart
+++ lib/component/facebook_signin_btn.dart
@@ -1,1 +1,14 @@
-class FacebookSigninBtn {}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FacebookSigninBtn {
+  static Widget of({
+    @required Widget child,
+    @required Color color,
+    @required Color textColor,
+    @required OnPressed onPressed,
+  }) =>
+      RaisedButton(child: child, onPressed: onPressed);
+}
+
+typedef Future OnPressed();
