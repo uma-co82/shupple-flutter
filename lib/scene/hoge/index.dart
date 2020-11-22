47b6b19977d8db5939456906d6694426f39b25ba
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-22 15:37:02 +0900

    Create index.dart

---
+++ lib/scene/hoge/index.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class HogeScene extends StatefulWidget {
+  @override
+  _HogeSceneState createState() => _HogeSceneState();
+}
+
+class _HogeSceneState extends State<HogeScene> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
