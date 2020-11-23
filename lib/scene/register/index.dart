cc73b81dbbd41af02183d8f3b2631691f5ea2c86
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-23 23:34:50 +0900

    Update index.dart

--- lib/scene/register/index.dart
+++ lib/scene/register/index.dart
@@ -1,6 +1,7 @@
 import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shupple_flutter/domain/entity/user.dart';
 import 'package:shupple_flutter/domain/model/enum.dart';
 
 class RegisterScene extends StatefulWidget {
@@ -9,36 +10,36 @@ class RegisterScene extends StatefulWidget {
 }
 
 class _RegisterSceneState extends State<RegisterScene> {
+  String _name = "";
+  String _email = "";
+  DateTime _birthdate = DateTime(2002);
+  Gender _gender = Gender.Male;
+  Prefecture _prefecture = Prefecture.Hokkaido;
+  MatchingReason _matchingReason = MatchingReason.Renai;
+  String _profile = "";
+
+  void _showBirthdatePicker() async {
+    var picked = await showDatePicker(
+        context: context,
+        initialDate: _birthdate,
+        firstDate: DateTime(2002),
+        initialDatePickerMode: DatePickerMode.year,
+        lastDate: DateTime.now(),
+        locale: Locale('ja'),
+        builder: (BuildContext context, Widget child) {
+          return Theme(
+            data: ThemeData.dark(),
+            child: child,
+          );
+        });
+    if (picked != null)
+      setState(() {
+        _birthdate = picked;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    String _name = "";
-    String _email = "";
-    DateTime _birthdate = DateTime(2002);
-    Gender _gender = Gender.Male;
-    Prefecture _prefecture = Prefecture.Hokkaido;
-    MatchingReason _matchingReason = MatchingReason.Renai;
-    String _profile = "";
-
-    void _showBirthdatePicker() async {
-      var picked = await showDatePicker(
-          context: context,
-          initialDate: _birthdate,
-          firstDate: DateTime(2002),
-          initialDatePickerMode: DatePickerMode.year,
-          lastDate: DateTime.now(),
-          locale: Locale('ja'),
-          builder: (BuildContext context, Widget child) {
-            return Theme(
-              data: ThemeData.dark(),
-              child: child,
-            );
-          });
-      if (picked != null)
-        setState(() {
-          _birthdate = picked;
-        });
-    }
-
     return Scaffold(
       appBar: AppBar(),
       body: Form(
@@ -64,21 +65,30 @@ class _RegisterSceneState extends State<RegisterScene> {
             DropdownButton(
                 value: EnumToString.convertToString(_gender),
                 items: Gender.values
-                    .map<DropdownMenuItem<String>>((e) => DropdownMenuItem(
+                    .map((e) => DropdownMenuItem(
                           child: Text(e.jpLabel),
                           value: EnumToString.convertToString(e),
                         ))
                     .toList(),
-                onChanged: (val) {}),
+                onChanged: (val) {
+                  setState(() {
+                    _gender = EnumToString.fromString(Gender.values, val);
+                  });
+                }),
             DropdownButton(
                 value: EnumToString.convertToString(_prefecture),
                 items: Prefecture.values
-                    .map<DropdownMenuItem<String>>((e) => DropdownMenuItem(
+                    .map((e) => DropdownMenuItem(
                           child: Text(e.jpLabel),
                           value: EnumToString.convertToString(e),
                         ))
                     .toList(),
-                onChanged: (val) {}),
+                onChanged: (val) {
+                  setState(() {
+                    _prefecture =
+                        EnumToString.fromString(Prefecture.values, val);
+                  });
+                }),
             DropdownButton(
                 value: EnumToString.convertToString(_matchingReason),
                 items: MatchingReason.values
@@ -87,7 +97,12 @@ class _RegisterSceneState extends State<RegisterScene> {
                           value: EnumToString.convertToString(e),
                         ))
                     .toList(),
-                onChanged: (val) {}),
+                onChanged: (val) {
+                  setState(() {
+                    _matchingReason =
+                        EnumToString.fromString(MatchingReason.values, val);
+                  });
+                }),
             TextFormField(
               decoration: InputDecoration(labelText: "プロフィール"),
               onChanged: (value) {
@@ -96,6 +111,17 @@ class _RegisterSceneState extends State<RegisterScene> {
                 });
               },
             ),
+            RaisedButton(
+                child: Text("登録"),
+                onPressed: () {
+                  var hoge = UserRegisterRequest(
+                      firebaseToken: "",
+                      name: _name,
+                      email: _email,
+                      gender: EnumToString.convertToString(_gender),
+                      birthdate: _birthdate.toString(),
+                      prefecture: EnumToString.convertToString(_prefecture));
+                })
           ],
         ),
       ),
