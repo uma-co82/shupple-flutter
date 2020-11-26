9592580cf87d0d4535b3cf4ba9bdd2c2c0fe5963
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-26 17:49:05 +0900

    Update index.dart

--- lib/scene/register/index.dart
+++ lib/scene/register/index.dart
@@ -44,99 +44,115 @@ class _RegisterSceneState extends State<RegisterScene> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: "ユーザー名"),
-              onChanged: (value) {
-                setState(() {
-                  _name = value;
-                });
-              },
-              validator: (String val) => SValidator.nameValidator(val),
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "email"),
-              onChanged: (value) {
-                setState(() {
-                  _email = value;
-                });
-              },
-              validator: (String val) => SValidator.emailValidator(val),
-            ),
-            RaisedButton(child: Text("誕生日"), onPressed: _showBirthdatePicker),
-            DropdownButton(
-                value: EnumToString.convertToString(_gender),
-                items: Gender.values
-                    .map((e) => DropdownMenuItem(
-                          child: Text(e.jpLabel),
-                          value: EnumToString.convertToString(e),
-                        ))
-                    .toList(),
-                onChanged: (val) {
-                  setState(() {
-                    _gender = EnumToString.fromString(Gender.values, val);
-                  });
-                }),
-            DropdownButton(
-                value: EnumToString.convertToString(_prefecture),
-                items: Prefecture.values
-                    .map((e) => DropdownMenuItem(
-                          child: Text(e.jpLabel),
-                          value: EnumToString.convertToString(e),
-                        ))
-                    .toList(),
-                onChanged: (val) {
-                  setState(() {
-                    _prefecture =
-                        EnumToString.fromString(Prefecture.values, val);
-                  });
-                }),
-            DropdownButton(
-                value: EnumToString.convertToString(_matchingReason),
-                items: MatchingReason.values
-                    .map<DropdownMenuItem<String>>((e) => DropdownMenuItem(
-                          child: Text(e.jpLabel),
-                          value: EnumToString.convertToString(e),
-                        ))
-                    .toList(),
-                onChanged: (val) {
-                  setState(() {
-                    _matchingReason =
-                        EnumToString.fromString(MatchingReason.values, val);
-                  });
-                }),
-            TextFormField(
-              decoration: InputDecoration(labelText: "プロフィール"),
-              onChanged: (value) {
-                setState(() {
-                  _profile = value;
-                });
-              },
-              validator: (val) => SValidator.profileValidator(val),
-            ),
-            RaisedButton(
-                child: Text("登録"),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    var hoge = UserRegisterRequest(
-                        firebaseToken: "",
-                        name: _name,
-                        email: _email,
-                        gender: EnumToString.convertToString(_gender),
-                        birthdate: _birthdate.toString(),
-                        prefecture: EnumToString.convertToString(_prefecture),
-                        matchingReason:
-                            EnumToString.convertToString(_matchingReason),
-                        profile: _profile);
-                    print(hoge.toJson());
-                  }
-                })
-          ],
-        ),
-      ),
+      body: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Form(
+            key: _formKey,
+            child: Flexible(
+                child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: "ユーザー名",
+                              labelStyle: TextStyle(fontSize: 12.0)),
+                          onChanged: (value) {
+                            setState(() {
+                              _name = value;
+                            });
+                          },
+                          validator: (String val) =>
+                              SValidator.nameValidator(val),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "email"),
+                          onChanged: (value) {
+                            setState(() {
+                              _email = value;
+                            });
+                          },
+                          validator: (String val) =>
+                              SValidator.emailValidator(val),
+                        ),
+                        RaisedButton(
+                            child: Text("誕生日"),
+                            onPressed: _showBirthdatePicker),
+                        DropdownButton(
+                            value: EnumToString.convertToString(_gender),
+                            items: Gender.values
+                                .map((e) => DropdownMenuItem(
+                                      child: Text(e.jpLabel),
+                                      value: EnumToString.convertToString(e),
+                                    ))
+                                .toList(),
+                            onChanged: (val) {
+                              setState(() {
+                                _gender =
+                                    EnumToString.fromString(Gender.values, val);
+                              });
+                            }),
+                        DropdownButton(
+                            value: EnumToString.convertToString(_prefecture),
+                            items: Prefecture.values
+                                .map((e) => DropdownMenuItem(
+                                      child: Text(e.jpLabel),
+                                      value: EnumToString.convertToString(e),
+                                    ))
+                                .toList(),
+                            onChanged: (val) {
+                              setState(() {
+                                _prefecture = EnumToString.fromString(
+                                    Prefecture.values, val);
+                              });
+                            }),
+                        DropdownButton(
+                            value:
+                                EnumToString.convertToString(_matchingReason),
+                            items: MatchingReason.values
+                                .map<DropdownMenuItem<String>>((e) =>
+                                    DropdownMenuItem(
+                                      child: Text(e.jpLabel),
+                                      value: EnumToString.convertToString(e),
+                                    ))
+                                .toList(),
+                            onChanged: (val) {
+                              setState(() {
+                                _matchingReason = EnumToString.fromString(
+                                    MatchingReason.values, val);
+                              });
+                            }),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "プロフィール"),
+                          onChanged: (value) {
+                            setState(() {
+                              _profile = value;
+                            });
+                          },
+                          validator: (val) => SValidator.profileValidator(val),
+                        ),
+                        RaisedButton(
+                            child: Text("登録"),
+                            onPressed: () {
+                              if (_formKey.currentState.validate()) {
+                                var hoge = UserRegisterRequest(
+                                    firebaseToken: "",
+                                    name: _name,
+                                    email: _email,
+                                    gender:
+                                        EnumToString.convertToString(_gender),
+                                    birthdate: _birthdate.toString(),
+                                    prefecture: EnumToString.convertToString(
+                                        _prefecture),
+                                    matchingReason:
+                                        EnumToString.convertToString(
+                                            _matchingReason),
+                                    profile: _profile);
+                                print(hoge.toJson());
+                              }
+                            })
+                      ],
+                    ))))
+      ]),
     );
   }
 }
