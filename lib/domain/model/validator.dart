645aa656e733c47cfb34919686cd2a05055edd5c
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2020-11-26 17:16:04 +0900

    Update validator.dart

--- lib/domain/model/validator.dart
+++ lib/domain/model/validator.dart
@@ -1,1 +1,11 @@
-class SValidator {}
+class SValidator {
+  static String nameValidator(String val) {
+    if (val.isEmpty) {
+      return "ユーザー名を入力して下さい";
+    }
+  }
+
+  static String emailValidator(String val) {
+    if
+  }
+}
